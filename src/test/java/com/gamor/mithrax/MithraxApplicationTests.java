d25d1f21df25515c7c4dfc8af84704623c3fa5a2
romeoraj369 <romeoraj0123@gmail.com>
2025-09-20 14:54:45 +0530

    first commit

---
+++ src/test/java/com/gamor/mithrax/MithraxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamor.mithrax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MithraxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
